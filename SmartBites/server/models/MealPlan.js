941df3dc14835684e66c72df7c357990d11605e7
vedha-333 <vedantpandilwad@gmail.com>
2025-05-25 20:02:33 +0530

    Implement relationship between entities in database

--- SmartBites/server/models/MealPlan.js
+++ SmartBites/server/models/MealPlan.js
@@ -4,7 +4,7 @@ const mealPlanSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  },
+  }, 
   preferences: {
     diet: String,
     calories: Number,
--- SmartBites/server/models/Order.js
+++ SmartBites/server/models/Order.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   },
-  items: [{
+  items: [{ 
     name: String,
     quantity: Number,
     price: Number,
